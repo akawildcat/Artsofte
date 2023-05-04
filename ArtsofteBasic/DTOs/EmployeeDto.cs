cfc0afa0e4abc177eb6fb118f7b8827b978448f4
Tatiana Vishnyakova <akawildcat@gmail.com>
2023-05-04 22:52:25 +0300

    get department by id

---
+++ ArtsofteBasic/DTOs/EmployeeDto.cs
@@ -0,0 +1,44 @@
+﻿using ArtsofteBasic.Domain;
+
+namespace ArtsofteBasic.DTOs;
+
+/// <summary>
+/// ДТО сотрудника
+/// </summary>
+public class EmployeeDto
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public Guid Id { get; init; }
+
+    /// <summary>
+    /// Name
+    /// </summary>
+    public string Name { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Surname
+    /// </summary>
+    public string Surname { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Age
+    /// </summary>
+    public int Age { get; init; }
+
+    /// <summary>
+    /// Gender
+    /// </summary>
+    public Genders Gender { get; init; }
+
+    /// <summary>
+    /// Department
+    /// </summary>
+    public string DepartmentName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Programming language
+    /// </summary>
+    public string ProgrammingLanguageName { get; set; } = string.Empty;
+}
---
+++ ArtsofteBasic/Logic/Services/EmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using ArtsofteBasic.Domain;
+using ArtsofteBasic.DTOs;
+
+namespace ArtsofteBasic.Logic.Services
+{
+    public class EmployeeService 
+    {
+        public IQueryable<EmployeeDto> DomainToDtos(IQueryable<Employee> deps)
+        {
+            var dtos = deps.Select(employee => new EmployeeDto()
+            {
+                Id = employee.Id
+            });
+            return dtos;
+        }
+
+        public EmployeeDto DomainToDto(Employee employee)
+        {
+            return DomainToDtos(new[] { employee }.AsQueryable()).Single();
+        }
+    }
+}
